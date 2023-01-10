47eb91240b11c2e995bcccd559eaaa1bba0bd439
Joon-Jeremy-Chun <joonchun1000@gmail.com>
2023-01-10 16:05:26 +0900

    test1

--- Visualization/section 1; data visualiztion.R
+++ Visualization/section 1; data visualiztion.R
@@ -1,3 +1,17 @@
 # Types of data
 # Categorical data; Ordinal categorical data or Non-ordinal categorical data
 # Numerical data; Continuous variables or Discrete variables
+
+```{r}
+prop.table(table(heights$sex))
+
+```
+
+
+
+library(dslabs)
+data(heights)
+
+#make a table of category proportions
+prop.table(table(heights$sex))
+#Outcome: table; Female and Male proportions
