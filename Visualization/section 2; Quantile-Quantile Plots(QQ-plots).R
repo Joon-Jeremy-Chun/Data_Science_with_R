18a36bcb7e20909109c75c96a2760cd279171ec1
Joon-Jeremy-Chun <joonchun1000@gmail.com>
2023-01-11 17:05:37 +0900

    JAN11-03

---
+++ Visualization/section 2; Quantile-Quantile Plots(QQ-plots).R
@@ -0,0 +1,28 @@
+#Objectives: To understand how to use Quantile-Quantile Plots(QQ-plots)
+
+# define x and z
+library(tidyverse)
+library(dslabs)
+data(heights)
+index <- heights$sex=="Male"
+x <- heights$height[index]
+z <- scale(x)
+
+# proportion of data below 69.5
+mean(x <= 69.5)
+#Outcome:0.5147783
+
+# calculate observed and theoretical quantiles
+p <- seq(0.05, 0.95, 0.05)
+observed_quantiles <- quantile(x, p)
+theoretical_quantiles <- qnorm(p, mean = mean(x), sd = sd(x))
+# make QQ-plot
+plot(theoretical_quantiles, observed_quantiles)
+abline(0,1)
+#Outcome: The table; its meaning -> each observed quantiles represent as each dots. And the line for theoretical 
+
+# make QQ-plot with scaled values
+observed_quantiles <- quantile(z, p)
+theoretical_quantiles <- qnorm(p) 
+plot(theoretical_quantiles, observed_quantiles)
+abline(0,1)
