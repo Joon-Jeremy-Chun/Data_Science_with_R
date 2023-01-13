8bc2fcab14cd1b8aac5b3d87efb06c18af266a2f
Joon-Jeremy-Chun <joonchun1000@gmail.com>
2023-01-13 14:47:30 +0900

    JAN13-01

---
+++ Visualization/section 2; graph components.R
@@ -0,0 +1,4 @@
+#ggplot2 consist of three component:
+#Data: The dataset being summarized
+#Geometry: The type of polt (scatterplot, boxplot, histogram, qqplot, smooth density, ets.)
+#Aesthetic mapping: Variables mapped to visual cuse, such as x-axis values and color
