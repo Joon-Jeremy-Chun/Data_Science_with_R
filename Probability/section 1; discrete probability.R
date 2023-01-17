b898792615d8193f146635f1d95145dc94d8c22a
Joon-Jeremy-Chun <joonchun1000@gmail.com>
2023-01-17 14:58:59 +0900

    JAN17-02

--- Probability/section 1; discrete probability.R
+++ Probability/section 1; discrete probability.R
@@ -1,4 +1,8 @@
+#Objectives and note;
 #Pr(A)= probability of event A
+#An important application of the mean function
+#Sampling- sample(); with and without replacement
+
 
 #Monte Carlo simulations for categorical data
 beads <- rep(c("red", "blue"), times = c(9,6))    # create an urn with 9 red, 6 blue
@@ -20,3 +24,26 @@ prop.table(tab)    # view table of outcome proportions
 #events
 #blue    red 
 #0.3984 0.6016 
+
+#use of the mean function
+beads
+
+#To find probability for blue beads
+beads == "blue"
+#Outcome: *randomly ... FALSE (or) TRUE...
+mean(beads == "blue")
+#Outcome:0.4
+
+#When the mean function is applied, R coerces the logical values to numeric values, changing TRUE to 1 and FALSE to 0.
+
+
+#sampling; with and without replacement
+sample(beads,1)
+#Outcome: *one random sample
+sample(beads,15)
+#Outcome: *respectively 15 samples
+sample(beads,16)
+#Error in sample.int(length(x), size, replace, prob) : 
+#cannot take a sample larger than the population when 'replace = FALSE'
+
+sample(beads, 16, replace = TRUE) # sampling with replacement
