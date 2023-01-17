633b62d6486e2f085695c10e299f0a4fa6748f89
Joon-Jeremy-Chun <joonchun1000@gmail.com>
2023-01-17 16:44:22 +0900

    JAN17-03

--- useful_math-statistics_codes/basic random sampling.R
+++ useful_math-statistics_codes/basic random sampling.R
@@ -1,11 +1,9 @@
-#Objectives and note;
-#Pr(A)= probability of event A
 #An important application of the mean function
 #Sampling- sample(); with and without replacement
-
-
 #Monte Carlo simulations for categorical data
-beads <- rep(c("red", "blue"), times = c(9,6))    # create an urn with 9 red, 6 blue
+
+
+beads <- rep(c("red", "green"), times = c(3,6))
 beads    # view beads object
 
 sample(beads, 1)    # sample 1 bead at random
@@ -17,34 +15,42 @@ tab <- table(events)    # make a table of outcome counts
 tab    # view count table
 #Outcome: table
 #events
-#blue  red 
-#3984 6016 
+#green   red 
+#6681  3319 
 prop.table(tab)    # view table of outcome proportions
 #Outcome: table
 #events
-#blue    red 
-#0.3984 0.6016 
+#green    red 
+#0.6681 0.3319 
 
 #use of the mean function
 beads
 
-#To find probability for blue beads
-beads == "blue"
+#To find probability for red beads
+beads == "red"
 #Outcome: *randomly ... FALSE (or) TRUE...
-mean(beads == "blue")
-#Outcome:0.4
+mean(beads == "red")
+#Outcome:0.3333333
 
 #When the mean function is applied, R coerces the logical values to numeric values, changing TRUE to 1 and FALSE to 0.
 
+#sampling with replacement and without replacement
 
 #sampling; with and without replacement
 sample(beads,1)
 #Outcome: *one random sample
-sample(beads,15)
-#Outcome: *respectively 15 samples
-sample(beads,16)
+sample(beads,9)
+#Outcome: *respectively 9 samples
+sample(beads,10)
 #Error in sample.int(length(x), size, replace, prob) : 
 #cannot take a sample larger than the population when 'replace = FALSE'
 
-sample(beads, 16, replace = TRUE) # sampling with replacement
-#Outcome: *16 random sample from the urn <- replacement
+sample(beads, 10, replace = TRUE) # sampling with replacement
+#Outcome: 10 random sample with replacement
+
+#Checking
+b_table<- replicate(10000, sample(beads, 10, replace = TRUE))
+table(b_table)
+#Outcome: *randomly but in the ratio
+#green   red 
+#66388 33612 
