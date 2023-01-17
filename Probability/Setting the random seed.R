6dbd20750e6e0144c5ba552236e401fb86825876
Joon-Jeremy-Chun <joonchun1000@gmail.com>
2023-01-17 14:37:37 +0900

    JAN17-01

---
+++ Probability/Setting the random seed.R
@@ -0,0 +1,13 @@
+# sampling with R (or other statistics programs) will generate the random numbers.
+# It does mean that the result of code will differ every time.
+# In order to have same result use the set.seed() function.
+
+
+#?set.seed
+
+set.seed(1)
+set.seed(1, sample.kind="Rounding")    # will make R 3.6 generate a seed as in R 3.5
+
+#Using the sample.kind="Rounding" argument will generate a message:
+  
+#non-uniform 'Rounding' sampler used
