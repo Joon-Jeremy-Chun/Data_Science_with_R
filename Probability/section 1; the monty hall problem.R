56074dedb78a56c27e6a7c20dfb9c6f9099461f3
Joon Jeremy Chun <joonchun1000@gmail.com>
2023-01-27 16:38:21 +0900

    JAN27-03

---
+++ Probability/section 1; the monty hall problem.R
@@ -0,0 +1,34 @@
+#from the text book - 'introduction to data science, R A,IRIZARRY'
+#Monte Carlo simulations can be used to simulate random outcomes, which makes them useful when exploring ambiguous or less intuitive problems like the Monty Hall problem.
+#In the Monty Hall problem, contestants choose one of three doors that may contain a prize. Then, one of the doors that was not chosen by the contestant and does not contain a prize is revealed. The contestant can then choose whether to stick with the original choice or switch to the remaining unopened door.
+#Although it may seem intuitively like the contestant has a 1 in 2 chance of winning regardless of whether they stick or switch, Monte Carlo simulations demonstrate that the actual probability of winning is 1 in 3 with the stick strategy and 2 in 3 with the switch strategy.
+
+
+#Code: Monte Carlo simulation of stick strategy
+B <- 10000
+stick <- replicate(B, {
+  doors <- as.character(1:3)
+  prize <- sample(c("car","goat","goat"))    # puts prizes in random order
+  prize_door <- doors[prize == "car"]    # note which door has prize
+  my_pick  <- sample(doors, 1)    # note which door is chosen
+  show <- sample(doors[!doors %in% c(my_pick, prize_door)],1)    # open door with no prize that isn't chosen
+  stick <- my_pick    # stick with original door
+  stick == prize_door    # test whether the original door has the prize
+})
+mean(stick)    # probability of choosing prize door when sticking
+#[1] 0.3269
+
+
+#Code: Monte Carlo simulation of switch strategy
+
+switch <- replicate(B, {
+  doors <- as.character(1:3)
+  prize <- sample(c("car","goat","goat"))    # puts prizes in random order
+  prize_door <- doors[prize == "car"]    # note which door has prize
+  my_pick  <- sample(doors, 1)    # note which door is chosen first
+  show <- sample(doors[!doors %in% c(my_pick, prize_door)], 1)    # open door with no prize that isn't chosen
+  switch <- doors[!doors%in%c(my_pick, show)]    # switch to the door that wasn't chosen first or opened
+  switch == prize_door    # test whether the switched door has the prize
+})
+mean(switch)    # probability of choosing prize door when switching
+#[1] 0.6698
