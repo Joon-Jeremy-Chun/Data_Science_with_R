eac3640d5cb40e3eb29ff2413131934c881799ba
Joon Jeremy Chun <joonchun1000@gmail.com>
2023-01-31 16:52:38 +0900

    JAN31-01

---
+++ R_Basics/section 2.5; the pipe.R
@@ -0,0 +1,11 @@
+16 %>% sqrt()
+#Error in 16 %>% sqrt() : could not find function "%>%"
+
+#The pipe is an operator in 'dplyr' library
+#Sending the result of one function to another using what is called 'the pipe operator' 
+library(dplyr)
+16 %>% sqrt()
+#[1] 4
+
+16 %>% sqrt() %>% log2()
+#[1] 2
