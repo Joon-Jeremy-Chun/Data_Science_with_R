51f4cd726061ecb5618dff293ff61361bea56e97
Joon Jeremy Chun <joonchun1000@gmail.com>
2023-04-13 18:00:19 +0900

    APR13-03

--- Data_wrangling/Data_import.R
+++ Data_wrangling/Data_import.R
@@ -14,3 +14,14 @@ path <- system.file("extdata", package = "dslabs") # assign the location in 'the
 list.files(path)
 
 
+#generate a full path to a file
+filename <- "murders.csv"
+fullpath <- file.path(path, filename)
+fullpath
+
+# copy file from dslabs package to your working directory
+file.copy(fullpath, getwd())
+
+# check
+file.exists(filename)
+#[1] TRUE
---
+++ Data_wrangling/Read_data-readr_and_readxl_Packages.R
@@ -0,0 +1,31 @@
+#Objectives-from the text book
+#readr is the tidyverse library that includes functions for reading data stored in text file spreadsheets into R. Functions in the package include read_csv(), read_tsv(), read_delim() and more. These differ by the delimiter they use to split columns.
+
+library(dslabs)
+library(tidyverse) #includes readr
+library(readxl)
+
+#inspect the first 3 lines
+read_lines("murders.csv", n_max = 3)
+
+#
+filename <- "murders.csv"
+path <- system.file("extdata", package = "dslabs")
+fullpath <- file.path(path, filename)
+
+#read file in csv format
+dat <- read_csv(filename)
+
+#read using full path
+dat <- read_csv(fullpath)
+head(dat)
+
+#Ex):
+path <- system.file("extdata", package = "dslabs")
+files <- list.files(path)
+files
+
+filename <- "HRlist2.txt"
+
+dat=read.table(file.path(path, filename))
+dat
