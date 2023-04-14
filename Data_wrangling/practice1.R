c5dc8e4bd8ce89c48398e998616717581bb7dae5
Joon Jeremy Chun <joonchun1000@gmail.com>
2023-04-14 14:55:51 +0900

    APR14-03

--- Data_wrangling/practice1.R
+++ Data_wrangling/practice1.R
@@ -7,21 +7,48 @@ path <- system.file("extdata", package = "dslabs")
 
 #1
 file.copy(file.path(path, "murders.csv"), getwd())
-#[1] FALSE
+#[1] TRUE
+#copy in the current folder
 
 #2
 #setwd("C:/XXX.../data")
 file.copy(file.path(path, filename), getwd())
 #[1] TRUE
+#copy in the current folder .../data
 list.files()
 #[1] "murders.csv"
 
 #3
 file.copy(file.path(path, "murders.csv"), file.path(getwd(), "data"))
 #[1] TRUE
+#copy in the data folder where sub of current one
 
 #4
 file.location <- file.path(system.file("extdata", package = "dslabs"), "murders.csv")
 file.destination <- file.path(getwd(), "data")
 file.copy(file.location, file.destination)
-#[1] FALSE
+#[1] TRUE
+#copy in that file in the assign path
+
+#
+#
+
+path
+list.files(path)
+file.copy(file.path(path, "HRlist2.txt"), file.path(getwd(), "data"))
+
+dat2 <- read_delim(file.path(path, "HRlist2.txt"))
+dat3 <- read.csv(file.path(path, "HRlist2.txt"))
+dat4 <- read_csv(file.path(path, "HRlist2.txt"))
+dat5 <- read_csv(file.path(path, "HRlist2.txt"), col_name = TRUE)
+
+#
+#
+
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+dat1 <- read_table(url)
+dat2 <- read_csv(url)
+dat3 <- read_csv2(url)
+dat4 <- read_tsv(url)
+
+dat2 <- read_csv(url, col_names = FALSE)
