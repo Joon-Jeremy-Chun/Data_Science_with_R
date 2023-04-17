16e1a57d66475a7051fe7f75abc3d57a3e395859
Joon Jeremy Chun <joonchun1000@gmail.com>
2023-04-17 12:53:38 +0900

    APR17-01

--- Data_wrangling/Reshaping_data-separate_and_unite.R
+++ Data_wrangling/Reshaping_data-separate_and_unite.R
@@ -2,6 +2,7 @@
 #The separate() function splits one column into two or more columns at 'a specified character' that separates the variables.
 #The separate() function takes three arguments (apart from the data): (1)the name of the column to be separate, (2)the names to be used for the new columns, (3)and the character that separates the variables.
 #When there is an extra separation, you can use extra = "merge" to merge the last two variables.
+#The unite() function joins two columns into one.
 
 library(tidyverse)
 library(dslabs)
@@ -39,3 +40,27 @@ dat4 <- dat %>% separate(name, c("year", "name"), sep = "_", extra = "merge", co
 dat5 <- dat %>% separate(name, c("year", "name"), sep = "_", extra = "merge", convert = TRUE) %>% pivot_wider()
 head(dat5)
 #Outcome: columns -> country, year, fertility, life_expectancy
+
+#
+#
+#if we had used this non-optimal approach to separate
+data_1 <- dat %>%
+  separate(name, c("year", "name_1", "name_2"), fill = 'right', convert = TRUE)
+#output: same results with dat3
+
+#we could unite the second and third columns using unite()
+data_2 <- dat %>%
+  separate(name, c("year", "name_1", "name_2"), fill = 'right', convert = TRUE) %>%
+  unite(variable_name, name_1, name_2, sep = "_")
+head(data_2)
+#1 Germany  1960 fertility_NA     2.41
+#2 Germany  1960 life_expectancy 69.3 
+
+#spread the columns
+data_3 <- dat %>%
+  separate(name, c("year", "name_1", "name_2"), fill = 'right', convert = TRUE) %>%
+  unite(name, name_1, name_2, sep = "_") %>%
+  spread(name, value) %>%
+  rename(fertility = fertility_NA)
+head(data_3)
+#outcome: we got same result with dat5
---
+++ Data_wrangling/practice2.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(dslabs)
+
+path <- system.file("extdata", package="dslabs")
+filename <- file.path(path, "times.csv")
+d<- read_csv(filename)
+
+list.files(path)
+read_csv("co2")
+list.files()
+head(co2)
+co2
