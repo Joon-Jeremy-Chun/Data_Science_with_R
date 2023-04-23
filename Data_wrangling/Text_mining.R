9ad508ab27cae125da60f4167899d6b8b8d9b87c
Joon Jeremy Chun <joonchun1000@gmail.com>
2023-04-23 14:00:40 +0900

    APR23-01

---
+++ Data_wrangling/Text_mining.R
@@ -0,0 +1,5 @@
+#Objectives, notes from the textbook.
+#The tidytext package helps us convert free form text into a tidy table.
+#Use unnest_tokens() to extract individual words and other meaningful chunks of text.
+#Sentiment analysis assigns emotions or a positive/negative score to tokens. You can extract sentiments using get_setiments(). Common lexicons for sentiment analysis are "bing", "afinn", "nrc" and "loughran".
+
