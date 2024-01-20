32f9cf75c691f80fe568fa00e9fec9ed8d48d520
Joon Jeremy Chun <joonchun1000@gmail.com>
2024-01-19 16:15:18 -0800

    JAN19-02

---
+++ Linear_regression/1.7 Exercise with mother and daughter heights.R
@@ -0,0 +1,11 @@
+set.seed(1989, sample.kind="Rounding") 
+library(HistData)
+data("GaltonFamilies")
+
+female_heights <- GaltonFamilies%>%     
+  filter(gender == "female") %>%     
+  group_by(family) %>%     
+  sample_n(1) %>%     
+  ungroup() %>%     
+  select(mother, childHeight) %>%     
+  rename(daughter = childHeight)
