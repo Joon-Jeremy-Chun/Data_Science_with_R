96ee2b5c1de5318fa27144e383482c9a2d9ad369
Joon Jeremy Chun <joonchun1000@gmail.com>
2024-06-18 16:14:23 -0700

    JUN18-01

---
+++ Linear_regression/2.7 Advanced dplyr-summarize with functions and broom.R
@@ -0,0 +1,66 @@
+#key points from the textbook
+#Tibbles can be regarded as a modern version of data frames and are the default data structure in the tidyverse.
+#Some functions that do not work properly with data frames do work with tibbles.
+#Including a function inside a summarize can help that function handle a grouped tibble.
+#The broom package has three main functions, tidy, glance, and augment, that are useful for connecting lm to the tidyverse.
+
+library(Lahman)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(broom)
+
+# stratify by HR
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR = round(HR/G, 1), 
+         BB = BB/G,
+         R = R/G) %>%
+  select(HR, BB, R) %>%
+  filter(HR >= 0.4 & HR<=1.2)
+
+# calculate slope of regression lines to predict runs by BB in different HR strata
+dat %>%  
+  group_by(HR) %>%
+  summarize(slope = cor(BB,R)*sd(R)/sd(BB))
+
+# use lm to get estimated slopes - lm does not work with grouped tibbles
+dat %>%  
+  group_by(HR) %>%
+  lm(R ~ BB, data = .) %>%
+  .$coef
+
+# include the lm inside a summarize and it will work
+dat %>%  
+  group_by(HR) %>%
+  summarize(slope = lm(R ~ BB)$coef[2])
+
+# tidy function from broom returns estimates in and information in a data frame
+fit <- lm(R ~ BB, data = dat)
+tidy(fit)
+
+# add confidence intervals
+tidy(fit, conf.int = TRUE)
+
+# combine with group_by and summarize to get the table we want
+dat %>%  
+  group_by(HR) %>%
+  summarize(tidy(lm(R ~ BB), conf.int = TRUE))
+
+# it's a data frame so we can filter and select the rows and columns we want
+dat %>%  
+  group_by(HR) %>%
+  summarize(tidy(lm(R ~ BB), conf.int = TRUE)) %>%
+  filter(term == "BB") %>%
+  select(HR, estimate, conf.low, conf.high)
+
+# visualize the table with ggplot
+dat %>%  
+  group_by(HR) %>%
+  summarize(tidy(lm(R ~ BB), conf.int = TRUE)) %>%
+  filter(term == "BB") %>%
+  select(HR, estimate, conf.low, conf.high) %>%
+  ggplot(aes(HR, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  geom_errorbar() +
+  geom_point()
+
+
--- Linear_regression/Practice-3.R
+++ Linear_regression/Practice-3.R
@@ -1,5 +1,10 @@
 #Practice3
 library(Lahman)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+options(digits = 3)
+
 bat_02 <- Batting %>% filter(yearID == 2002) %>%
   mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
   filter(pa >= 100) %>%
@@ -9,3 +14,21 @@ bat_01 <- Batting %>% filter(yearID %in% 1999:2001 ) %>%
   mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
   filter(pa >= 100) %>%
   select(playerID, singles, bb)
+
+mean_bat02 <- bat_02 %>% group_by(playerID) %>%
+  summarise(across(everything(), mean, na.rm = TRUE))
+
+mean_bat01 <- bat_01 %>% group_by(playerID) %>%
+  summarise(across(everything(), mean, na.rm = TRUE))
+
+mean_singles_bat01 <- bat_01 %>% group_by(playerID) %>%
+  summarise(across(everything(), mean, na.rm = TRUE)) %>%
+  filter(singles > 0.2)
+
+mean_bb_bat01 <- bat_01 %>% group_by(playerID) %>%
+  summarise(across(everything(), mean, na.rm = TRUE)) %>%
+  filter(bb > 0.2)
+
+joined_data <- inner_join(mean_bat01, mean_bat02, by = "playerID")
+correlation_singles <- cor(joined_data$singles.x, joined_data$singles.y)
+correlation_bb <- cor(joined_data$bb.x, joined_data$bb.y)
