270753c896dd9eadfd13c847699a5eb8e5672edd
Joon Jeremy Chun <joonchun1000@gmail.com>
2024-06-23 12:56:23 -0700

    JUN23-01

---
+++ Machine_learning/1.1 Introduction to Machine Learning.R
@@ -0,0 +1,10 @@
+#1.1Introduction to Machine Learning
+
+#Data comes in the form of features and outcomes.
+
+#We want to build an algorithm that takes feature values as input and returns a prediction when we don’t know the actual outcome. 
+#We will train algorithms using datasets for which we do know the actual outcome, and then apply these algorithms in the future to make predictions when we don’t know the actual outcome.
+#This is also referred to as supervised learning.
+
+#Supervised learning problems can be divided into those with categorical outcomes, which we refer to as classification; and those with continuous outcomes, which we refer to as prediction.
+# X_1, ..., X_p denote the features/predictors/covariates, Y denotes the outcomes, and Y_hat denotes the predictions.
